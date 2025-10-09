e7e980ce0e742842781ef5698ba21648ef7bcd54
Marty Stew <marty.stew@protonmail.com>
2025-10-09 10:45:26 -0400

    Filler text

--- scripts/deployindex.py
+++ scripts/deployindex.py
@@ -3,33 +3,30 @@ import sys
 from datetime import datetime
 from string import Template
 
-def deploy_section(section):
+def deploy_index():
     datemodified = datetime.now().isoformat()
     current_year = datetime.now().year
-    file = open(os.path.expanduser('~/.marty/') + 'txt/' + section + '/index.org', 'r')
+    file = open(os.path.expanduser('~/.marty/') + 'txt/index.org', 'r')
     section_index = file.readlines()    
     content = '<ul>\n'
     for i in range(len(section_index)):
         if section_index[i].startswith("* "):
             link_title = section_index[i].removeprefix("* ").strip()
             link_summary = section_index[i+1].removeprefix("** ").strip()
-            link_url = f'/{section}/' + section_index[i+2].removeprefix("** ").strip() + '.html'
+            link_url = '/' + section_index[i+2].removeprefix("** ").strip() + '/'
             content += f'        <li><a href="{link_url}"><h2>{link_title}</h2><p>{link_summary}</p></a></li>\n'
         elif section_index[i].startswith("#+DESCRIPTION: "):
             description = section_index[i].removeprefix("#+DESCRIPTION: ").strip()
     content += '      </ul>';
 
-    with open(os.path.expanduser('~/.marty/') + 'templates/section.html', 'r') as f:
+    with open(os.path.expanduser('~/.marty/') + 'templates/index.html', 'r') as f:
         template_content = f.read()
 
     template = Template(template_content)
-    result = template.substitute(content=content, datemodified=datemodified, description=description, section=section.title(), current_year=current_year)
+    result = template.substitute(content=content, current_year=current_year, datemodified=datemodified, description=description)
 
-    if not os.path.exists(os.path.expanduser('~/.marty/docs/') + section):
-        os.makedirs(os.path.expanduser('~/.marty/docs/') + section)        
-    with open(os.path.expanduser('~/.marty/docs/') + section + '/index.html', 'w') as f:
+    with open(os.path.expanduser('~/.marty/docs/index.html'), 'w') as f:
         f.write(result)
 
-folder = sys.argv[1]        
-deploy_section(folder)
+deploy_index()
         
